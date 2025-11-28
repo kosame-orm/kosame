28eee303073b126eed2e36e2ba4d8759812538d8
Julien Scholz <8947616+pikaju@users.noreply.github.com>
2025-11-28 21:09:13 +0100

    Add DSL part for SelectCore

--- kosame_dsl/src/clause/select.rs
+++ kosame_dsl/src/clause/select.rs
@@ -2,7 +2,13 @@ use proc_macro2::TokenStream;
 use quote::{ToTokens, quote};
 use syn::parse::{Parse, ParseStream};
 
-use crate::{clause::Fields, keyword, visitor::Visitor};
+use crate::{
+    clause::{Fields, From, GroupBy, Having, Where},
+    keyword,
+    parse_option::ParseOption,
+    quote_option::QuoteOption,
+    visitor::Visitor,
+};
 
 pub struct Select {
     pub select_keyword: keyword::select,
@@ -10,10 +16,6 @@ pub struct Select {
 }
 
 impl Select {
-    pub fn peek(input: ParseStream) -> bool {
-        input.peek(keyword::select)
-    }
-
     pub fn accept<'a>(&'a self, visitor: &mut impl Visitor<'a>) {
         self.fields.accept(visitor);
     }
@@ -28,11 +30,82 @@ impl Parse for Select {
     }
 }
 
+impl ParseOption for Select {
+    fn peek(input: ParseStream) -> bool {
+        input.peek(keyword::select)
+    }
+}
+
 impl ToTokens for Select {
     fn to_tokens(&self, tokens: &mut TokenStream) {
         let fields = &self.fields;
         quote! {
             ::kosame::repr::clause::Select::new(#fields)
+        }
+        .to_tokens(tokens);
+    }
+}
+
+pub struct SelectCore {
+    pub select: Select,
+    pub from: Option<From>,
+    pub r#where: Option<Where>,
+    pub group_by: Option<GroupBy>,
+    pub having: Option<Having>,
+}
+
+impl SelectCore {
+    pub fn accept<'a>(&'a self, visitor: &mut impl Visitor<'a>) {
+        self.select.accept(visitor);
+        if let Some(inner) = self.from.as_ref() {
+            inner.accept(visitor);
+        }
+        if let Some(inner) = self.r#where.as_ref() {
+            inner.accept(visitor);
+        }
+        if let Some(inner) = self.group_by.as_ref() {
+            inner.accept(visitor);
+        }
+        if let Some(inner) = self.having.as_ref() {
+            inner.accept(visitor);
+        }
+    }
+}
+
+impl Parse for SelectCore {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        Ok(Self {
+            select: input.parse()?,
+            from: input.call(From::parse_option)?,
+            r#where: input.call(Where::parse_option)?,
+            group_by: input.call(GroupBy::parse_option)?,
+            having: input.call(Having::parse_option)?,
+        })
+    }
+}
+
+impl ParseOption for SelectCore {
+    fn peek(input: ParseStream) -> bool {
+        Select::peek(input)
+    }
+}
+
+impl ToTokens for SelectCore {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        let select = &self.select;
+        let from = QuoteOption::from(&self.from);
+        let r#where = QuoteOption::from(&self.r#where);
+        let group_by = QuoteOption::from(&self.group_by);
+        let having = QuoteOption::from(&self.having);
+
+        quote! {
+            ::kosame::repr::clause::SelectCore::new(
+                #select,
+                #from,
+                #r#where,
+                #group_by,
+                #having,
+            )
         }
         .to_tokens(tokens);
     }
